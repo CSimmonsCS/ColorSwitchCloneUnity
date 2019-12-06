b1e2f75ef301953a81bf698b5b850ef2fc834e47
Christian Simmons <csimmonscs@gmail.com>
2019-12-05 18:28:43 -0800

    Uploading Color Switch Clone to Github

---
+++ Color Switch Clone/Assets/Scripts/Player.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float jumpForce = 10f;
+
+    public Rigidbody2D rb;
+    public SpriteRenderer sr;
+
+    public string currentColor;
+
+    public Color colorCyan;
+    public Color colorYellow;
+    public Color colorPink;
+    public Color colorMagenta;
+
+    private void Start()
+    {
+        SetRandomColor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "ColorChanger")
+        {
+            SetRandomColor();
+            Destroy(col.gameObject);
+            return;
+        }
+
+        Debug.Log(col.tag);
+        if(col.tag != currentColor)
+        {
+            Debug.Log("GAME OVER");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void SetRandomColor()
+    {
+        int index = Random.Range(0, 4);
+
+        switch (index)
+        {
+            case 0:
+                currentColor = "Cyan";
+                sr.color = colorCyan;
+                break;
+            case 1:
+                currentColor = "Yellow";
+                sr.color = colorYellow;
+                break;
+            case 2:
+                currentColor = "Pink";
+                sr.color = colorPink;
+                break;
+            case 3:
+                currentColor = "Magenta";
+                sr.color = colorMagenta;
+                break;
+            default:
+                break;
+        }
+    }
+}
